6592267ef8fb98d342db4cd94bd4b83ed50d4dc3
Andrew Gomoll <andrewgomoll1@gmail.com>
2023-07-20 13:52:29 -0400

    everything except history

--- assets/js/source.js
+++ assets/js/source.js
@@ -4,22 +4,51 @@ function GETWeather() {
     var cityName = document.getElementById("CityName");
     cityName.innerHTML = newName.value;
 
+
 fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=942fa7e731b18136008887a82567b4d9')
 .then(response => response.json())
 .then(data => {
 
     for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min -218).toFixed(1)+ "°F";
+        document.getElementById("day" + (i+1) + "Temp").innerHTML = "Temp: " + Number(data.list[i].main.temp -218).toFixed(1)+ "°F";
     }
 
     for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max -218).toFixed(2) + "°F";
+        document.getElementById("day" + (i+1) + "Wind").innerHTML = "Wind: " + Number(data.list[i].wind.speed).toFixed(2) + "MPH";
     }
 
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Humidity").innerHTML = "Humidity: " + Number(data.list[i].main.humidity).toFixed(0) + "%";
+    }
+    
+
+    document.getElementById("currentTemp").innerHTML = "Temp: " + Number(data.list[0].main.temp -218).toFixed(2) + "°F";
+    document.getElementById("currentWind").innerHTML = "Wind: " + Number(data.list[0].wind.speed).toFixed(2) + "MPH";
+    document.getElementById("currentHumidity").innerHTML = "Humidity: " + Number(data.list[0].main.humidity).toFixed(0);
+
+    history(newName.value);
+
     console.log(data)
+
+    
 
 
 })
 
-.catch(err => alert("Error"))
+function history(newName)
+{
+var name = newName;
+var names = [];
+names.push(name);
+
+const localStorageContent = localStorage.getItem(names)
+
+console.log(localStorageContent)
+
+for(i=0; i < names.length; i++)
+{
+document.getElementById("lastSearch"+i).innerHTML = name;
+}
+}
+//.catch(err => alert("Error"))
 }
